f6dc515ee8cdffcf0f165fad576a5126561fe709
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2016-10-13 18:24:00 +0200

    .

--- src/app/code/Mgt/DeveloperToolbar/view/adminhtml/web/js/blocks.js
+++
@@ -1,8 +0,0 @@
-define([
-    'jquery'
-], function ($) {
-    'use strict';
-    $('.mgt-developer-toolbar-sidebar-block-parent a').click(function() {
-        $(this).next('ul.mgt-developer-toolbar-sidebar-block-properties').toggle(); 
-    });
-});
